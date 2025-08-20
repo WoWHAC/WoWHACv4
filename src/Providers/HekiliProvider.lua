35ce96521861256f58afee692a2af1e900b97835
CKATEPTb <ckateptb@vivaldi.net>
2025-08-20 16:30:21 +0300

    Refactor `HekiliProvider.lua` to track and report current and next actions by keybind.

--- src/Providers/HekiliProvider.lua
+++ src/Providers/HekiliProvider.lua
@@ -2,16 +2,31 @@ local _, WoWHACv4 = ...
 
 local HekiliProvider = WoWHACv4.Provider:extend("HekiliProvider")
 
+local currentId;
+local currentHotkey;
+local nextId;
+local nextHotkey;
 function HekiliProvider:init()
     WoWHACv4:Log("Supplier found: Hekili.")
     WoWHACv4:RegisterMessage("WOWHACV4_WA_PRESENTS", function(_, _, isLoaded)
         if isLoaded then
-            WoWHACv4:SecureHook(WeakAuras, "ScanEvents", function(event, _, _, _, _, arg5)
+            WoWHACv4:SecureHook(WeakAuras, "ScanEvents", function(event, display, spellId, _, _, arg5)
                 if event == "HEKILI_RECOMMENDATION_UPDATE" then
                     if arg5 then
-                        if arg5[1] then
-                            if arg5[1].keybind then
-                                self:Fire(arg5[1].keybind)
+                        local current = arg5[1]
+                        if current then
+                            local keybind = current.keybind
+                            if keybind ~= currentHotkey then
+                                currentHotkey = keybind
+                                currentId = spellId
+                                local next = arg5[2]
+                                if next then
+                                    local nextKeybind = next.keybind
+                                    if nextKeybind ~= nextHotkey then
+                                        nextHotkey = nextKeybind
+                                        nextId = next.actionID
+                                    end
+                                end
                             end
                         end
                     end
@@ -21,6 +36,22 @@ function HekiliProvider:init()
             WoWHACv4:Log("To use the Hekili as rotation, you need to install WeakAuras.")
         end
     end)
+end
+
+function HekiliProvider:GetCurrentHotKey()
+    return currentHotkey
+end
+
+function HekiliProvider:GetCurrentId()
+    return currentId
+end
+
+function HekiliProvider:GetNextHotKey()
+    return nextHotkey
+end
+
+function HekiliProvider:GetNextId()
+    return nextId
 end
 
 WoWHACv4.providers["Hekili"] = HekiliProvider
