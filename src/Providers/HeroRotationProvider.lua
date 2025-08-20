6c1a34f74b540c582fe0504114b12e9b36929992
CKATEPTb <ckateptb@vivaldi.net>
2025-08-20 17:06:57 +0300

    Refactor hero rotation provider to track and report the current hotkey. Add methods to fetch the current hotkey and ID.

--- src/Providers/HeroRotationProvider.lua
+++ src/Providers/HeroRotationProvider.lua
@@ -2,23 +2,48 @@ local _, WoWHACv4 = ...
 
 local HeroRotationProvider = WoWHACv4.Provider:extend("HeroRotationProvider")
 
+local currentHotkey;
 function HeroRotationProvider:init()
     WoWHACv4:Log("Supplier found: HeroRotation.")
     local Keybind = HeroRotation.MainIconFrame.Keybind
     local last = Keybind:GetText()
     WoWHACv4:SecureHook(Keybind, "SetText", function(_, txt)
+        local rightSuggest = HeroRotation.RightSuggestedIconFrame.Keybind
+        if rightSuggest and rightSuggest:IsVisible() then
+            local suggestedHotkey = rightSuggest:GetText()
+            if suggestedHotkey and suggestedHotkey ~= "" then
+                currentHotkey = suggestedHotkey
+                return
+            end
+        end
+        local suggested = HeroRotation.SuggestedIconFrame.Keybind
+        if suggested and suggested:IsVisible() then
+            local suggestedHotkey = suggested:GetText()
+            if suggestedHotkey and suggestedHotkey ~= "" then
+                currentHotkey = suggestedHotkey
+                return
+            end
+        end
         local burst = HeroRotation.SmallIconFrame.Icon[1].Keybind
         if burst and burst:IsVisible() then
             local burstHotkey = burst:GetText()
             if burstHotkey and burstHotkey ~= "" then
-                txt = burstHotkey
+                currentHotkey = burstHotkey
+                return
             end
         end
         if txt ~= last then
-            last = txt
-            self:Fire(txt)
+            currentHotkey = txt
         end
     end)
+end
+
+function HeroRotationProvider:GetCurrentHotKey()
+    return currentHotkey
+end
+
+function HeroRotationProvider:GetCurrentId()
+    return 0
 end
 
 WoWHACv4.providers["HeroRotation"] = HeroRotationProvider
