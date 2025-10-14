1c73b7932c0a7ed430c4e7525be7a4fec66d110e
CKATEPTb <ckateptb@vivaldi.net>
2025-10-14 18:34:25 +0300

    Initialize `ToggleHekiliFrame` in `HekiliProvider.lua`.

--- src/Providers/HekiliProvider.lua
+++ src/Providers/HekiliProvider.lua
@@ -3,6 +3,8 @@ local _, WoWHACv5 = ...
 WoWHACv5.providers = WoWHACv5.providers or {}
 WoWHACv5.providers["Hekili"] = function()
     WoWHACv5:Log("Supplier found: Hekili.")
+    WoWHACv5.ToggleHekiliFrame:Init()
+
     local provider = Hekili_Primary_B1;
     local Keybind = provider.Keybinding
 
